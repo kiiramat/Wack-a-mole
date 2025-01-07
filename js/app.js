c91bfbe36ae5d8c2d694c3d928b01adf68d03516
kiiramat <kiiramati@gmail.com>
2025-01-07 16:09:05 +0000

    Add score

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 const board = document.querySelector(".container");
 
+function scoreDisplay(board) {
+    const scoreDiv = document.createElement("div");
+    scoreDiv.setAttribute("class", "score");
+    const scoreText = document.createElement("h2");
+    scoreText.innerHTML = "Your Score:";
+    const scoreValue = document.createElement("h2");
+    scoreValue.innerHTML = 0;
+
+    board.append(scoreDiv);
+    scoreDiv.append(scoreText);
+    scoreDiv.append(scoreValue);
+}
+
+scoreDisplay(board)
+
 function drawBoard(board, numDivs) {
     const grid = document.createElement("div");
     grid.setAttribute("class", "grid");
