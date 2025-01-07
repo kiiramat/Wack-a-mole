37e586f11f31aa3e9e7d7ad14324a7780ba1e5fa
kiiramat <kiiramati@gmail.com>
2025-01-07 16:20:50 +0000

    Added class attributes to ScoreDiv and timerDiv

--- js/app.js
+++ js/app.js
@@ -2,10 +2,11 @@ const board = document.querySelector(".container");
 
 function scoreDisplay(board) {
     const scoreDiv = document.createElement("div");
-    scoreDiv.setAttribute("class", "score");
+    scoreDiv.setAttribute("class", "scoreDiv");
     const scoreText = document.createElement("h2");
     scoreText.innerHTML = "Your Score:";
     const scoreValue = document.createElement("h2");
+    scoreValue.setAttribute("class", "score");
     scoreValue.innerHTML = 0;
 
     board.append(scoreDiv);
@@ -17,10 +18,11 @@ scoreDisplay(board);
 
 function timerDisplay(board) {
     const timerDiv = document.createElement("div");
-    timerDiv.setAttribute("class", "timer");
+    timerDiv.setAttribute("class", "timerDiv");
     const timerText = document.createElement("h2");
     timerText.innerHTML = "Time left:";
     const timerValue = document.createElement("h2");
+    timerValue.setAttribute("class", "time-left");
     timerValue.innerHTML = 60;
 
     board.append(timerDiv);
