c37a89bab3118df8be136727d8353a26ae5b1146
kiiramat <kiiramati@gmail.com>
2025-01-08 13:00:22 +0000

    Refactor code

--- js/app.js
+++ js/app.js
@@ -3,14 +3,14 @@ const board = document.querySelector(".container");
 function scoreDisplay(board) {
     const scoreDiv = document.createElement("div");
     scoreDiv.setAttribute("class", "scoreDiv");
-    const scoreText = document.createElement("h2");
-    scoreText.innerHTML = "Your Score:";
+    const scoreLabel = document.createElement("h2");
+    scoreLabel.innerHTML = "Your Score:";
     const scoreValue = document.createElement("h2");
     scoreValue.setAttribute("class", "score");
     scoreValue.innerHTML = 0;
 
     board.append(scoreDiv);
-    scoreDiv.append(scoreText);
+    scoreDiv.append(scoreLabel);
     scoreDiv.append(scoreValue);
 }
 
@@ -19,14 +19,14 @@ scoreDisplay(board);
 function timerDisplay(board) {
     const timerDiv = document.createElement("div");
     timerDiv.setAttribute("class", "timerDiv");
-    const timerText = document.createElement("h2");
-    timerText.innerHTML = "Time left:";
+    const timerLabel = document.createElement("h2");
+    timerLabel.innerHTML = "Time left:";
     const timerValue = document.createElement("h2");
     timerValue.setAttribute("class", "time-left");
     timerValue.innerHTML = 60;
 
     board.append(timerDiv);
-    timerDiv.append(timerText);
+    timerDiv.append(timerLabel);
     timerDiv.append(timerValue);
 }
 
